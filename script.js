2797df155e952f24f74d322014dd16345d37d032
kshama101 <kshama101@gmail.com>
2022-09-30 01:16:58 -0700

    First Commit

--- script.js
+++ script.js
@@ -8,18 +8,74 @@ navLinks.forEach((l) => {
 })
 
 
-// addEventListner() method execute a function (validate) when user clicks on the submit 
-const submit = document.getElementById("submit");
+// addEventListner() method execute a function (validate) when a user clicks on the submit 
+ const submit = document.getElementById("submit");
+ submit.addEventListener("click", validate);
 
-submit.addEventListener("click", validate);
+ // Accessing the elements with specific id, and assigning to variables
+ const nameField = document.getElementById("name");
+ const emailField = document.getElementById("email");
+ const subjectField = document.getElementById("subject");
+ const commentField = document.getElementById("comment");
+ 
+// addEventListner() method execute a function (updateValue) when a user provides an input
+ nameField.addEventListener("input", updateValue);
+ emailField.addEventListener("input", updateValue);
+ subjectField.addEventListener("input", updateValue);
+ commentField.addEventListener("input", updateValue);
 
+ // function for email validation that uses  regular expressions
+ const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+// function to show or hide the error based on user input
+ function updateValue(e) {
+    const errorMsg = document.getElementById(e.target.id+"Error");
+    hide_error(e.target, errorMsg)
+    if (e.target.id==='email')
+      {
+        const emailError2 = document.getElementById("emailError2");
+        if (!validateEmail(e.target.value))
+          show_error(emailField, emailError2);
+        else
+          hide_error(emailField, emailError2);
+        
+      }
+}
+
+// Function to show the error
+function show_error(elmField,elmError)
+{
+  elmError.classList.add("visible");
+  elmField.classList.add("invalid");
+  elmError.setAttribute("aria-hidden", false);
+  elmError.setAttribute("aria-invalid", true);
+}
+
+// Function to hide the error
+function hide_error(elmField,elmError)
+{
+  elmError.classList.remove("visible");
+  elmField.classList.remove("invalid");
+  elmError.setAttribute("aria-hidden", true);
+  elmError.setAttribute("aria-invalid", false);
+  console.log(elmField.value) 
+}
+
+
+// function for form validation
 function validate(e) {
   e.preventDefault();
   
   const nameField = document.getElementById("name");
   const emailField = document.getElementById("email");
   const subjectField = document.getElementById("subject");
-  const textField = document.getElementById("comment");
+  const commentField = document.getElementById("comment");
   let valid = true;
 
   // if field is empty then making the span visible showing the error messege, also adding invalid class to 
@@ -27,43 +83,54 @@ function validate(e) {
   // Or if field has value then removing the span's visible class, which shows the error msg, also removing the 
   // field's invalid class which removes the red border
 
-  if (!nameField.value) {
-    const nameError = document.getElementById("nameError");
-    nameError.classList.add("visible");
-    nameField.classList.add("invalid");
+  const nameError = document.getElementById("nameError");
  
-  }else if (nameField.value) 
-  {
-    const nameError = document.getElementById("nameError");
-    nameError.classList.remove("visible");
-    nameField.classList.remove("invalid");
-    console.log(nameField.value);  
-   
-  }
-  
-  if(!emailField.value){
-    const emailError = document.getElementById("emailError");
-    emailError.classList.add("visible");
-    emailField.classList.add("invalid");
- 
+    if (!nameField.value) {
+      show_error(nameField,nameError)
+    } 
+    else if (nameField.value) 
+    {
+    hide_error(nameField,nameError)
+    }
 
-  }
-  else if (emailField.value)
-  {
-    const emailError = document.getElementById("emailError");
-    emailError.classList.remove("visible");
-    emailError.classList.remove("invalid");
-    console.log(emailField.value);  
-  }
+  const emailError = document.getElementById("emailError");
+    if(!emailField.value ){
+      show_error(emailField,emailError)
+    } 
+    else if (emailField.value)
+    { if (validateEmail( emailField.value))
+        {  hide_error(emailField, emailError)
+          const emailError2 = document.getElementById("emailError2");
+          hide_error(emailField, emailError2)
+        }
+      else
+        {const emailError = document.getElementById("emailError2");
+          show_error(emailField, emailError);
+        }
+    }
 
-  if(subjectField.value){
-    console.log(subjectField.value)
-  }
+  const subjectError = document.getElementById("subjectError");
+    if(!subjectField.value){
+      show_error(subjectField,subjectError)
+    }
+    else if (subjectField.value)
+    {
+      hide_error(subjectField,subjectError)
+      
+    }
 
-  if(textField.value){
-    console.log(textField.value)
-}
+  const commentError = document.getElementById("commentError");
+    if(!commentField.value){
+      show_error(commentField,commentError)
+    }
+    else if (commentField.value)
+    {
+      hide_error(commentField,commentError)
+    }
 
   return valid;
 }
+
+
+
 
